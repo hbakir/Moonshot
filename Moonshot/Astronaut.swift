392f9fe64603012d4c65392e83f668c7fb99334c
Hicham BAKIR <hicham@bakir.ch>
2023-09-11 23:56:47 +0200

    Add mission and astronaut detail views

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Hicham Bakir on 11.09.23.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
